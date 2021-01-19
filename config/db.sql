2013dec3d892c40bd42b5a4a379f81ab94854cab
Mr. Lan <fdevops@163.com>
2021-01-19 23:27:59 +0800

    feat: update db.sql。

--- config/db.sql
+++ config/db.sql
@@ -165,6 +165,7 @@ INSERT INTO sys_menu VALUES (503, 'ResourceCloudAccount', '云账户', 'people',
 INSERT INTO sys_menu VALUES (504, 'ResourceCloudArea', '云区域', 'textarea', '/cmdb/resource/cloud-area', '/0/484/494/504', 'C', '', '', 494, 0, '', '/cmdb/resource/cloud-area', 3, '0', '1', '', '1', '2021-01-14 12:39:16.640', '2021-01-14 12:39:16.640', null);
 INSERT INTO sys_menu VALUES (505, 'ResourceCloudResource', '云资源发现', 'upload', '/cmdb/resource/cloud-resource', '/0/484/494/505', 'C', '', '', 494, 0, '', '/cmdb/resource/cloud-resource', 4, '0', '1', '', '1', '2021-01-14 12:40:55.473', '2021-01-14 12:40:55.473', null);
 INSERT INTO sys_menu VALUES (506, 'ResourceEventpush', '事件订阅', 'rate', '/cmdb/resource/eventpush', '/0/484/494/506', 'C', '', '', 494, 0, '', '/cmdb/resource/eventpush', 5, '0', '1', '', '1', '2021-01-14 12:42:49.600', '2021-01-14 12:42:49.600', null);
+INSERT INTO sys_menu VALUES (507, 'ModelConfigurationFields', '模型字段', 'guide', '/cmdb/model/model-fields', '/0/484/496/507', 'C', '', '', 496, 0, '', '/cmdb/model/model-fields', 4, '1', '1', '', '1', '2021-01-18 22:22:30.841000000', '2021-01-18 22:22:30.841000000', null);
 COMMIT;
 
 -- 数据完成 ;
