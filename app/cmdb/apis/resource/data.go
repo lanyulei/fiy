336b6a6290056060ab07cac600deb526ba1d0c59
YuleiLan <yllan@aibee.com>
2021-02-03 15:36:06 +0800

    feat: 资源详情接口开发。

--- app/cmdb/apis/resource/data.go
+++ app/cmdb/apis/resource/data.go
@@ -3,6 +3,7 @@ package resource
 import (
 	"fiy/app/cmdb/models/resource"
 	orm "fiy/common/global"
+	"fiy/common/pagination"
 	"fiy/tools/app"
 
 	"github.com/gin-gonic/gin"
@@ -12,12 +13,32 @@ import (
   @Author : lanyulei
 */
 
-// 获取业务模型详情
-func GetBizDetails(c *gin.Context) {
-	app.OK(c, nil, "")
+// 获取数据列表
+func DataList(c *gin.Context) {
+	var (
+		err      error
+		dataList []*resource.Data
+		result   interface{}
+		modelId  string
+	)
+
+	modelId = c.Param("id")
+
+	db := orm.Eloquent.Model(&resource.Data{}).Where("info_id = ?", modelId)
+
+	result, err = pagination.Paging(&pagination.Param{
+		C:  c,
+		DB: db,
+	}, &dataList)
+	if err != nil {
+		app.Error(c, -1, err, "分页查询关联类型列表失败")
+		return
+	}
+
+	app.OK(c, result, "")
 }
 
-// 新建业务数据
+// 新建数据
 func CreateData(c *gin.Context) {
 	var (
 		err  error
@@ -33,6 +54,52 @@ func CreateData(c *gin.Context) {
 	err = orm.Eloquent.Create(&data).Error
 	if err != nil {
 		app.Error(c, -1, err, "新建资源数据失败")
+		return
+	}
+
+	app.OK(c, nil, "")
+}
+
+// 删除数据
+func DeleteData(c *gin.Context) {
+	var (
+		err    error
+		dataId string
+	)
+
+	dataId = c.Param("id")
+
+	err = orm.Eloquent.Delete(&resource.Data{}, dataId).Error
+	if err != nil {
+		app.Error(c, -1, err, "删除资源数据失败")
+		return
+	}
+
+	app.OK(c, nil, "")
+}
+
+// 编辑数据
+func EditData(c *gin.Context) {
+	var (
+		err    error
+		data   resource.Data
+		dataId string
+	)
+
+	dataId = c.Param("id")
+
+	err = c.ShouldBind(&data)
+	if err != nil {
+		app.Error(c, -1, err, "参数绑定失败")
+		return
+	}
+
+	err = orm.Eloquent.Model(&data).Where("id = ?", dataId).Updates(map[string]interface{}{
+		"info_id": data.InfoId,
+		"data":    data.Data,
+	}).Error
+	if err != nil {
+		app.Error(c, -1, err, "更新资源数据失败")
 		return
 	}
 
--- app/cmdb/router/resource.go
+++ app/cmdb/router/resource.go
@@ -15,6 +15,9 @@ import (
 func RegisterCmdbResourceRouter(v1 *gin.RouterGroup, authMiddleware *jwt.GinJWTMiddleware) {
 	r := v1.Group("/cmdb/resource").Use(authMiddleware.MiddlewareFunc()).Use(middleware.AuthCheckRole())
 	{
-		r.POST("", resource.CreateData) // 新建资源数据
+		r.POST("", resource.CreateData)            // 新建资源数据
+		r.GET("/data/:id", resource.DataList)      // 资源数据列表
+		r.DELETE("/data/:id", resource.DeleteData) // 删除资源数据
+		r.PUT("/data/:id", resource.EditData)      // 编辑资源数据
 	}
 }
