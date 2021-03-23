6191fb3095e7dcff2f37be97548a1e7048032272
YuleiLan <yllan@aibee.com>
2021-03-23 11:29:29 +0800

    feat: 完善云资源同步。

--- app/cmdb/models/resource/data.go
+++ app/cmdb/models/resource/data.go
@@ -13,7 +13,7 @@ import (
 // 字段数据
 type Data struct {
 	Id     int            `gorm:"column:id; primary_key;AUTO_INCREMENT" json:"id"`                       // 字段分组ID
-	UUID   string         `gorm:"column:uuid; type:varchar(45); unique;" json:"uuid" binding:"required"` // 设备唯一ID
+	Uuid   string         `gorm:"column:uuid; type:varchar(45); unique;" json:"uuid" binding:"required"` // 设备唯一ID
 	InfoId int            `gorm:"column:info_id; type:int(11);" json:"info_id" binding:"required"`       // 对应的模型ID
 	Status int            `gorm:"column:status; type:int(11); default:1" json:"status"`                  // 0 没有状态，1 空闲，2 故障，3 待回收，4 正在使用
 	Data   datatypes.JSON `gorm:"column:data; type:json" json:"data" binding:"required"`                 // 数据
