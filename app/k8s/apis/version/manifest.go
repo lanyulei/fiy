97a16915dff27a2a3eb390f600e6920c8113e66f
lanyulei <fdevops@163.com>
2021-04-27 23:34:20 +0800

    feat: 完善版本管理接口。

--- app/k8s/apis/version/manifest.go
+++ app/k8s/apis/version/manifest.go
@@ -4,6 +4,7 @@ import (
 	"fiy/app/k8s/models/version"
 	orm "fiy/common/global"
 	"fiy/tools/app"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,5 +26,24 @@ func List(c *gin.Context) {
 		return
 	}
 
-	app.OK(c, list, "")
+	content := make([]map[string][]version.Manifest, 0)
+	tmpContent := make(map[string][]version.Manifest)
+	tmpVersionList := make([]string, 0)
+	for _, v := range list {
+		vs := strings.Join(strings.Split(v.Version, ".")[0:2], ".")
+		if _, ok := tmpContent[vs]; ok {
+			tmpContent[vs] = append(tmpContent[vs], v)
+		} else {
+			tmpContent[vs] = []version.Manifest{v}
+			tmpVersionList = append(tmpVersionList, vs)
+		}
+	}
+
+	for _, v := range tmpVersionList {
+		content = append(content, map[string][]version.Manifest{
+			v: tmpContent[v],
+		})
+	}
+
+	app.OK(c, content, "")
 }
